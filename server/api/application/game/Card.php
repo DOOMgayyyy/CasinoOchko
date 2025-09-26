54086d1c4a351533fed9f3c001f97fd8901f417d
. <178396577+lotoffivann@users.noreply.github.com>
2025-09-26 09:35:53 +0400

    classCardAndDeck
    Создал класс Crard и Deck. Реализовал функцию calculateHandScore в отдельном файле gameLogic.php. Создал файл test.php для проверки.

---
+++ server/api/application/game/Card.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * Класс Card - представляет игральную карту
+ * 
+ * Каждая карта имеет масть, ранг и базовое значение для подсчета очков.
+ * Используется в игре Очко (Блэкджек) для создания колоды и расчета очков.
+ */
+class Card {
+    /**
+     * @var string $suit Масть карты (hearts, diamonds, clubs, spades)
+     */
+    public $suit;
+    
+    /**
+     * @var string $rank Ранг карты (A, 2, 3, ..., 10, J, Q, K)
+     */
+    public $rank;
+    
+    /**
+     * @var int $value Базовое значение карты для подсчета очков
+     */
+    public $value;
+
+    /**
+     * Конструктор класса Card
+     * 
+     * Создает новую карту с указанной мастью и рангом.
+     * Автоматически рассчитывает базовое значение карты.
+     * 
+     * @param string $suit Масть карты
+     * @param string $rank Ранг карты
+     */
+    public function __construct($suit, $rank) {
+        $this->suit = $suit;
+        $this->rank = $rank;
+        $this->value = $this->calculateValue($rank);
+    }
+
+    /**
+     * Рассчитывает базовое значение карты по ее рангу
+     * 
+     * Правила расчета:
+     * - Туз (A) = 11 (может быть изменен на 1 в игровой логике)
+     * - Картинки (K, Q, J) = 10
+     * - Числовые карты = их номинал
+     * 
+     * @param string $rank Ранг карты
+     * @return int Базовое значение карты
+     */
+    private function calculateValue($rank) {
+        switch ($rank) {
+            case 'A':
+                return 11; # Туз по умолчанию 11, может быть изменен на 1
+            case 'K':
+            case 'Q':
+            case 'J':
+                return 10; # Все картинки стоят 10 очков
+            default:
+                return (int)$rank; # Числовые карты равны своему номиналу
+        }
+    }
+
+    /**
+     * Возвращает строковое представление карты
+     * 
+     * Формат: "ранг of масть" (например, "A of hearts")
+     * 
+     * @return string Строковое представление карты
+     */
+    public function __toString() {
+        return $this->rank . ' of ' . $this->suit;
+    }
+}
