4bba2b7d097c1eb0df150c38e2918f1c889bbac3
LubutelbUgr <gleb.kha3412@gmail.com>
2025-09-27 11:16:13 +0300

    Player

---
+++ server/api/application/game/Player.php
@@ -0,0 +1,51 @@
+<?php
+require_once('Card.php');
+require_once('gameLogic.php');
+
+class Player
+{
+    public $id;
+    public $cards = [];
+    public $balance;
+
+    public function __construct($id, $balance = 1000) //заглушка 1000 баланс 
+    {
+        $this->id = $id;
+        $this->balance = $balance;
+        $this->cards = [];
+    }
+
+    //добавление карты в руку
+    public function addCard(Card $card) 
+    {
+        $this->cards[] = $card;
+        return $this;
+    }
+
+    //расчет очков руки
+    public function getScore() 
+    {
+        return gameLogic::calculateHandScore($this->cards);
+    }
+
+    //делаем ставку если хватает денег. amount - сумма ставки 
+    public function makeBet($amount) 
+    {
+        if ($this->balance >= $amount) {
+            $this->balance -= $amount;
+            return $amount;
+        }
+        return 0;
+    }
+}
+
+//проверка на перебор руки
+function isBust($hand) 
+{
+    if ($hand instanceof Player) {
+        return $hand->getScore() > 21;
+    } elseif (is_array($hand)) {
+        return gameLogic::calculateHandScore($hand) > 21;
+    }
+    return false;
+}
